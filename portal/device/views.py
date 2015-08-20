cf9c285b1416510702151db0706fd8caf117601b
RYWU <b00902048@ntu.edu.tw>
2015-08-20 14:36:05 +0800

    Another Bug Bang!

--- portal/device/views.py
+++ portal/device/views.py
@@ -44,9 +44,13 @@ class APIHandler:
         return response.text;  # return API call html.  
         # return '{}'
 
-# ------------- Utility Functions Starts----------- #
-def debug_area(param1):
-    return param1;
+# ------------- Utility Functions Starts ----------- #
+def debug_area(device_id):
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Hardware' +'/'+ device_id + '/' + 'getObject' +'.json?objectMask=mask[operatingSystem.passwords]';
+    s = requests.Session();
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+    return item_dict['operatingSystem']['passwords'];    
 
 def count_device():
     return len( json.loads(APIHandler.functionHandler(APIHandler(serviceName = 'SoftLayer_Account', methodName = 'getHardware'))) ) + \
@@ -60,6 +64,15 @@ def list_baremetal():
                       mask='topLevelLocation',
                     ));
     item_dict = json.loads(ret);
+
+    for item in item_dict:
+        temp = item['provisionDate'].partition('T')[0] 
+        temp += ' '
+        temp += item['provisionDate'].partition('T')[2][:8]
+        temp += '(UTC'
+        temp += item['provisionDate'].partition('T')[2][8:] + ')'
+        item['provisionDate'] = temp
+
     return item_dict;  
 
 def list_hourly_virtual_server():
@@ -69,6 +82,15 @@ def list_hourly_virtual_server():
                       mask='location',
                     ));
     item_dict = json.loads(ret);
+
+    for item in item_dict:
+        temp = item['provisionDate'].partition('T')[0] 
+        temp += ' '
+        temp += item['provisionDate'].partition('T')[2][:8]
+        temp += '(UTC'
+        temp += item['provisionDate'].partition('T')[2][8:] + ')'
+        item['provisionDate'] = temp
+
     return item_dict;  
 
 def list_monthly_virtual_server():
@@ -78,10 +100,19 @@ def list_monthly_virtual_server():
                       mask='location',
                     ));
     item_dict = json.loads(ret);
+
+    for item in item_dict:
+        temp = item['provisionDate'].partition('T')[0] 
+        temp += ' '
+        temp += item['provisionDate'].partition('T')[2][:8]
+        temp += '(UTC'
+        temp += item['provisionDate'].partition('T')[2][8:] + ')'
+        item['provisionDate'] = temp
+
     return item_dict;
 
-def api_doc_md():
-    with open('device/device_list.md', 'r') as content_file:
+def api_doc_md(file_path):
+    with open(file_path, 'r') as content_file:
         ret = content_file.read();
     markdown2.markdown(ret)
     return markdown2.markdown(ret, extras=['tables']);
@@ -92,6 +123,14 @@ def get_device_info_by_id_baremetal(device_id):
     response = s.get(requestURL);
     item_dict = json.loads(response.text);
     return item_dict;
+
+def get_start_date(device_id):
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Hardware' +'/'+ device_id + '/' + 'getObject' +'.json';
+    s = requests.Session();
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+    date = item_dict['provisionDate'][:10] + ' ' + item_dict['provisionDate'][11:19] + '(UTC' + item_dict['provisionDate'][19:25] + ')';
+    return date;    
 
 def get_device_info_by_id_virtual_server(device_id):
     requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Hardware' +'/'+ device_id + '/' + 'getObject' +'.json';
@@ -143,14 +182,91 @@ def get_drive_controller(device_id):
     return item_dict;
 
 def get_baremetal_credential(device_id):
-    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Hardware' +'/'+ device_id + '/' + 'getRemoteManagementAccounts' +'.json';
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Hardware' +'/'+ device_id + '/' + 'getObject' +'.json?objectMask=mask[operatingSystem.passwords]';
+    s = requests.Session();
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+
+    for item in item_dict['operatingSystem']['passwords']:
+        temp = item['modifyDate'].partition('T')[0] 
+        temp += ' '
+        temp += item['modifyDate'].partition('T')[2][:8]
+        temp += '(UTC'
+        temp += item['modifyDate'].partition('T')[2][8:] + ')'
+        item['modifyDate'] = temp
+
+    return item_dict['operatingSystem']['passwords'];    
+
+def get_ip_address_baremetal(device_id):
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Account' +'/' + 'getHardware' +'.json';
+    s = requests.Session();
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+
+    for d in item_dict:
+        if d['id'] == int(device_id):
+            return d
+
+def get_default_gateway_baremetal(device_id):
+    ipAddr = []
+    ipAddr.append(get_ip_address_baremetal(device_id)['primaryIpAddress']);
+    ipAddr.append(get_ip_address_baremetal(device_id)['privateIpAddress']) 
+    ipAddr.append(get_ip_address_baremetal(device_id)['networkManagementIpAddress'])
+
+    ret = []
+    s = requests.Session()
+    
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Network_Subnet_IpAddress' +'/' + 'getByIpAddress' +'/'+ ipAddr[0] + '.json';
+    response = s.get(requestURL)
+    item_dict = json.loads(response.text)
+    ret.append(item_dict['subnet']['gateway'])
+
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Network_Subnet_IpAddress' +'/' + 'getByIpAddress' +'/'+ ipAddr[1] + '.json';
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+    ret.append(item_dict['subnet']['gateway'])
+
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Network_Subnet_IpAddress' +'/' + 'getByIpAddress' +'/'+ ipAddr[2] + '.json';
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+    ret.append(item_dict['subnet']['gateway'])
+    
+    return ret;
+
+def get_subnet_mask_baremetal(device_id):
+    ipAddr = []
+    ipAddr.append(get_ip_address_baremetal(device_id)['primaryIpAddress']);
+    ipAddr.append(get_ip_address_baremetal(device_id)['privateIpAddress']) 
+    ipAddr.append(get_ip_address_baremetal(device_id)['networkManagementIpAddress'])
+
+    ret = []
+    s = requests.Session()
+    
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Network_Subnet_IpAddress' +'/' + 'getByIpAddress' +'/'+ ipAddr[0] + '.json';
+    response = s.get(requestURL)
+    item_dict = json.loads(response.text)
+    ret.append(item_dict['subnet']['netmask'])
+
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Network_Subnet_IpAddress' +'/' + 'getByIpAddress' +'/'+ ipAddr[1] + '.json';
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+    ret.append(item_dict['subnet']['netmask'])
+
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Network_Subnet_IpAddress' +'/' + 'getByIpAddress' +'/'+ ipAddr[2] + '.json';
+    response = s.get(requestURL);
+    item_dict = json.loads(response.text);
+    ret.append(item_dict['subnet']['netmask'])
+    
+    return ret;
+
+def get_network_interface(device_id):
+    requestURL = 'https://'+settings.SL_USERNAME+':'+settings.SL_APIKEY+'@api.softlayer.com/rest/v3/'+ 'SoftLayer_Hardware' +'/'+ device_id + '/' + 'getObject' +'.json?objectMask=mask[primaryNetworkComponent,primaryBackendNetworkComponent,remoteManagementComponent]';
     s = requests.Session();
     response = s.get(requestURL);
     item_dict = json.loads(response.text);
     return item_dict;
 
 # ------------- Utility Functions Ends----------- #
-
 
 # ------------- URL-Handler Functions Starts----------- #
 def device(request):
@@ -160,12 +276,13 @@ def device_list(request):
     return render(request, 
                   ['api_and_function.html'], 
                   {
+                   # 'debug': debug_area(),
                    'total_devices_number': count_device(),
                    'baremetal': list_baremetal(),
                    'hourlyvirtualserver': list_hourly_virtual_server(),
                    'monthlyvirtualserver': list_monthly_virtual_server(),
                    # 'api_list': api_list(),
-                   'api_doc_md': api_doc_md(), 
+                   'api_doc_md': api_doc_md('device/device_list.md'), 
                   },
                 )
 
@@ -173,17 +290,22 @@ def device_detail_baremetal(request, device_id):
     return render(request, 
                   ['api_and_function.html'], 
                   {
-                   'debug': debug_area(device_id),
+                   # 'debug': debug_area(device_id),
+                   'device_info': get_device_info_by_id_baremetal(device_id),  
+                   'start_date': get_start_date(device_id),
                    'device_type': "Bare Metal",
                    'os': get_os_baremetal(device_id),
                    'ram': get_ram_baremetal(device_id),
                    'processor': get_processors(device_id),
                    'motherboard': get_motherboard(device_id), 
-                   'device_info': get_device_info_by_id_baremetal(device_id), 
                    'powersupply': get_power_supply(device_id),
                    'drivecontroller': get_drive_controller(device_id),
-                   'baremetal_username': get_baremetal_credential(device_id),
-                   'baremetal_password': get_baremetal_credential(device_id),
+                   'baremetal_credential': get_baremetal_credential(device_id),
+                   'ip_address_baremetal': get_ip_address_baremetal(device_id),
+                   'default_dateway_baremetal': get_default_gateway_baremetal(device_id),
+                   'subnet_mask_baremetal': get_subnet_mask_baremetal(device_id),
+                   'network_interface': get_network_interface(device_id),
+                   'api_doc_md': api_doc_md('device/device_detail_baremetal.md'),
                   },
                 )
 
@@ -191,7 +313,7 @@ def device_detail_virtual_server(request, device_id):
     return render(request, 
                   ['api_and_function.html'], 
                   {
-                   'debug': debug_area(device_id),
+                   # 'debug': debug_area(device_id),
                    'device_type': "Virtual Server",
                    'device_info': get_device_info_by_id_virtual_server(device_id), 
                   },
