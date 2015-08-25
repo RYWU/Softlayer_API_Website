1404727c24d0b7365d2bc53c887aa8be51425968
RYWU <b00902048@ntu.edu.tw>
2015-08-25 15:57:16 +0800

    Remove Portal dir

--- Portal/Ticket/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Ticket.models import Post
-
-admin.site.register(Post)
-
-# Register your models here.
--- Portal/static/password_mask.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('.pword').click(function() {
-    $('#'+this.id).attr('type', 'text');
-  });
-});         
